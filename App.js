0bbfaa452eb230dc91a5dd0748b0d443b7c545e8
RusselUc <6584@itescam.edu.mx>
2023-07-14 16:02:13 -0600

    feat: add QueryClientProvider

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import Button from './components/Button';
-import { createUser, signin } from './api/service';
-import axios from 'axios';
-import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Navigation from './Navigation';
+import AuthProvider from './context/AuthProvider';
 
+
+const queryClient = new QueryClient()
 export default function App() {
   return (
-    <Navigation />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
