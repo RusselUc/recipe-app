9266e59841e724ddda642604345b7535de10d525
RusselUc <6584@itescam.edu.mx>
2023-07-14 16:05:53 -0600

    refactor: add props of disabled and text

--- components/Button.js
+++ components/Button.js
@@ -1,29 +1,39 @@
-import { Fragment } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 const Button = (props) => {
+    const { disabled, text } = props;
+
     return (
-        <TouchableOpacity style={styles.button} {...props}>
-            <Text style={styles.text}>Entrar</Text>
+        <TouchableOpacity
+            style={[styles.button, disabled && styles.disabledButton]}
+            {...props}
+            disabled={disabled}
+        >
+            <Text style={[styles.text, disabled && styles.disabledText]}>{text}</Text>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-
     text: {
         fontSize: 14,
-        color: 'gray',
         color: '#fff'
     },
     button: {
         marginTop: 20,
-        width: '25%',
+        width: '60%',
         height: 50,
         backgroundColor: '#5A75FA',
         borderRadius: 10,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    disabledText: {
+        color: 'gray'
+    },
+    disabledButton: {
+        backgroundColor: 'lightgray',
+        // Aquí puedes agregar cualquier otro estilo que desees para un botón desactivado
     }
 })
 
