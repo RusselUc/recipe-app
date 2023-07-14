178f9fd210eb07c57fc95e2f39525eca289bbd5b
RusselUc <6584@itescam.edu.mx>
2023-07-14 16:07:24 -0600

    feat: add folder context and AuthProvider

---
+++ context/AuthProvider.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react"
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState('')
+    return (
+        <AuthContext.Provider value={{ token, setToken }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export default AuthProvider
