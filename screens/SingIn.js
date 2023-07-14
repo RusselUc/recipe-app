173b52f78014ffa83ce5db8a6a690870f8ba6dfd
RusselUc <6584@itescam.edu.mx>
2023-07-14 15:58:15 -0600

    feat: add SigninScreen

--- screens/SingIn.js
+++ screens/SingIn.js
@@ -1,23 +1,38 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Button from '../components/Button'
 import { StatusBar } from 'expo-status-bar'
 import { signin } from '../api/service'
 import { useNavigation } from '@react-navigation/native'
+import { useMutation } from 'react-query'
+import { AuthContext } from '../context/AuthProvider'
 
 const SingIn = () => {
-    const [data, setData] = useState({ email: '', password: '' })
+    const [form, setForm] = useState({ email: '', password: '' })
     const { navigate } = useNavigation()
+    const { setToken } = useContext(AuthContext)
+
+    const { mutate, data, error, isLoading } = useMutation(signin, {
+        onSuccess: async (data) => {
+            setToken(data?.token)
+            data && navigate("recipes")
+        }
+    })
+
+    const disabled = () => {
+        return form.email !== '' && form.password !== ''
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Recipe App</Text>
             <Text style={styles.subTitle}>Sign In to your account</Text>
-            <TextInput style={styles.textInput} placeholder='usuario@email.com' onChangeText={(text) => setData({ ...data, email: text })} />
-            <TextInput style={styles.textInput} placeholder='contraseña' secureTextEntry={true} onChangeText={(text) => setData({ ...data, password: text })} />
-            <Button onPress={() => signin(data)} />
+            <TextInput style={styles.textInput} placeholder='usuario@email.com' onChangeText={(text) => setForm({ ...form, email: text })} />
+            <TextInput style={styles.textInput} placeholder='contraseña' secureTextEntry={true} onChangeText={(text) => setForm({ ...form, password: text })} />
+            <Button onPress={() => mutate(form)} disabled={!disabled()} text="Entrar" />
 
-            <TouchableOpacity onPress={() => navigate("signup")}>
-                <Text> Crear cuenta </Text>
+            <TouchableOpacity style={styles.buttonSignup} onPress={() => navigate("signup")}>
+                <Text style={styles.text}> Crear cuenta</Text>
             </TouchableOpacity>
             <StatusBar style="auto" />
         </View>
@@ -41,6 +56,21 @@ const styles = StyleSheet.create({
     subTitle: {
         fontSize: 20,
         color: 'gray'
+    },
+
+    text: {
+        fontSize: 14,
+        color: '#fff'
+    },
+
+    buttonSignup: {
+        marginTop: 20,
+        width: '60%',
+        height: 50,
+        backgroundColor: '#00afff',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 
     textInput: {
