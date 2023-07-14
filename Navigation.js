cbb3eb12007f387da43a0cf736982ac78522110d
RusselUc <6584@itescam.edu.mx>
2023-07-14 16:03:07 -0600

    feat: add new route of recipes

--- Navigation.js
+++ Navigation.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import SingIn from "./screens/SingIn"
 import { NavigationContainer } from "@react-navigation/native"
 import SingUp from "./screens/SingUp"
+import Recipes from "./screens/Recipes"
 
 const mainStack = createNativeStackNavigator()
 
@@ -11,6 +12,7 @@ const Navigation = () => {
             <mainStack.Navigator initialRouteName="signin">
                 <mainStack.Screen name="signin" component={SingIn} options={{ headerShown: false }} />
                 <mainStack.Screen name="signup" component={SingUp} options={{ headerShown: false }} />
+                <mainStack.Screen name="recipes" component={Recipes} options={{ headerShown: false }} />
             </mainStack.Navigator>
         </NavigationContainer>
     )
