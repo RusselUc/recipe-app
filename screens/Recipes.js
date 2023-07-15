cb254b4d0fc77934b7054acf165f2a4e0cd1b8ee
RusselUc <russelarielucpuc@gmail.com>
2023-07-14 18:45:43 -0600

    refactor: add FlatList for data

--- screens/Recipes.js
+++ screens/Recipes.js
@@ -1,16 +1,19 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { FlatList, Image, SafeAreaView, StyleSheet } from 'react-native'
 import { View, Text } from 'react-native'
 import { useQuery } from 'react-query'
-import { getRecipes } from '../api/service'
+import { getRecipe, getRecipes } from '../api/service'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthProvider'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
-import Button from '../components/Button'
+import CameraComponent from '../components/CameraComponent'
+import { StatusBar } from 'expo-status-bar'
+import Recipe from '../components/Recipe'
 const Recipes = () => {
     const { token, setToken } = useContext(AuthContext)
     const { navigate } = useNavigation()
     const { data, status } = useQuery(['recipes', token], getRecipes)
+
 
     const logOut = () => {
         setToken('')
@@ -19,27 +22,42 @@ const Recipes = () => {
 
     return (
         <View style={styles.container}>
-            {status === 'success' &&
-                data.map((product) => (<Text key={product.id} style={styles.title}>{product.title}</Text>))
+            {status === 'success' && (
+                <SafeAreaView>
+                    <FlatList
+                        data={data}
+                        keyExtractor={(product) => product.id}
+                        renderItem={({ item }) => <Recipe {...item} />}
+                        ListHeaderComponent={() => <Text style={styles.title}>Recetas</Text>}
+                        contentContainerStyle={styles.contentContainerStyle} />
+                </SafeAreaView>
+            )
             }
-
-            <Button onPress={() => logOut()} text="Cerrar sesión" />
+            <StatusBar style="auto" />
         </View>
+        // <CameraComponent />
     )
 }
 
 const styles = StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     backgroundColor: '#f1f1f1',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    // },
+
     container: {
         flex: 1,
-        backgroundColor: '#f1f1f1',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#E9E9EF',
+        justifyContent: 'center'
     },
 
     title: {
-        fontSize: 20,
+        fontSize: 30,
         color: '#34434D',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginBottom: 10
     },
 
     text: {
@@ -54,6 +72,10 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+
+    contentContainerStyle: {
+        padding: 15
     }
 });
 
