aef289fc2a7bde63d30e6d9bf92b91318d341a34
RusselUc <russelarielucpuc@gmail.com>
2023-07-17 18:34:17 -0600

    refactor: add new mutation createRecipe

--- api/service.js
+++ api/service.js
@@ -28,12 +28,71 @@ export const getRecipes = async ({ queryKey }) => {
     return data
 }
 
+// export const getRecipe = async ({ queryKey }) => {
+//     const { data } = await axios.get(`${API}/recipe/recipes/${queryKey[2]}/`, {
+//         headers: {
+//             Authorization: `Token ${queryKey[1].toString()}`
+//         }
+//     })
+//     return data
+// };
+
 export const getRecipe = async ({ queryKey }) => {
-    const { data } = await axios.get(`${API}/recipe/recipes/${queryKey[2]}/`,
-        {
+    return new Promise((resolve) => {
+        setTimeout(async () => {
+            const { data } = await axios.get(`${API}/recipe/recipes/${queryKey[2]}/`, {
+                headers: {
+                    Authorization: `Token ${queryKey[1].toString()}`
+                }
+            });
+            resolve(data);
+        }, 2000); // 2000 milisegundos = 2 segundos
+    });
+};
+
+
+// export const createRecipe = async ({ mutationKey }) => {
+//     console.log(mutationKey[1])
+//     try {
+//         const { data } = await axios.post(`${API}/recipe/recipes/`, mutationKey[1], {
+//             headers: {
+//                 'Content-Type': 'multipart/form-data',
+//                 Authorization: `Token ${mutationKey[0]}`,
+//             }
+//         });
+//         return data;
+//     } catch (error) {
+//         console.log(error.response.data)
+//     }
+
+// }
+
+export const createRecipe = async ({ mutationKey }) => {
+    try {
+        const formData = new FormData();
+        formData.append('image', {
+            uri: mutationKey[1].image.uri,
+            name: 'image.jpg',
+            type: 'image/jpg',
+        });
+        formData.append('title', mutationKey[1].title);
+        formData.append('time_minutes', mutationKey[1].time_minutes);
+        formData.append('price', mutationKey[1].price);
+        formData.append('description', mutationKey[1].description);
+
+        console.log(formData)
+
+        const { data } = await axios.post(`${API}/recipe/recipes/`, formData, {
             headers: {
-                Authorization: `Token ${queryKey[1].toString()}`
-            }
-        })
-    return data
-}
+                'Content-Type': 'multipart/form-data',
+                Authorization: `Token ${mutationKey[0]}`,
+            },
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error.response.data);
+    }
+};
+
+
