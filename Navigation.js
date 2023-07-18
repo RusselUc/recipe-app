5cfbce82d170bfafd7332afa997a28e6a639d460
RusselUc <russelarielucpuc@gmail.com>
2023-07-17 18:33:46 -0600

    refactor: add supense for show skeleton

--- Navigation.js
+++ Navigation.js
@@ -4,8 +4,23 @@ import { NavigationContainer } from "@react-navigation/native"
 import SingUp from "./screens/SingUp"
 import Recipes from "./screens/Recipes"
 import SingleRecipe from "./screens/SingleRecipe"
+import { Suspense, lazy } from "react"
+import SkeletonDetails from "./components/SkeletonDetails"
+import RecipeForm from "./components/RecipeForm"
+import CameraComponent from "./components/CameraComponent"
 
 const mainStack = createNativeStackNavigator()
+
+const RecipeScreen = lazy(() => import('./screens/SingleRecipe'));
+
+
+const LazyDetails = () => {
+    return (
+        <Suspense fallback={<SkeletonDetails />}>
+            <RecipeScreen />
+        </Suspense>
+    )
+}
 
 const Navigation = () => {
     return (
@@ -14,7 +29,9 @@ const Navigation = () => {
                 <mainStack.Screen name="signin" component={SingIn} options={{ headerShown: false }} />
                 <mainStack.Screen name="signup" component={SingUp} options={{ headerShown: false }} />
                 <mainStack.Screen name="recipes" component={Recipes} options={{ headerShown: false }} />
-                <mainStack.Screen name="details" component={SingleRecipe} options={{ headerShown: false }} />
+                <mainStack.Screen name="details" component={LazyDetails} options={{ headerShown: false }} />
+                <mainStack.Screen name="recipe-form" component={RecipeForm} options={{ headerShown: false }} />
+                <mainStack.Screen name="camera" component={CameraComponent} options={{ headerShown: false }} />
             </mainStack.Navigator>
         </NavigationContainer>
     )
