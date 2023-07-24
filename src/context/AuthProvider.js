023550f478384f22e74398691cd111dfad1cbc49
RusselUc <russelarielucpuc@gmail.com>
2023-07-24 12:57:24 -0600

    feature: add useEffect for save token

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import { setAuthToken } from "../api/axiosInstance";
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
     const [token, setToken] = useState('')
+    useEffect(() => {
+        setAuthToken(token); // Configuramos el token en axiosInstance
+    }, [token]);
+
     return (
         <AuthContext.Provider value={{ token, setToken }}>
             {children}
